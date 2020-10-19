ea917aa99ffa5ae429e76b4052b0a64164947e54
Shea Phillips <shea.phillips@gmail.com>
2020-10-19 12:05:44 -0700

    Replacing provider reference with correct (for tf 0.13+) terraform required_providers block and updating provier version to current.

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-	version = "~> 2.0"
+terraform {
+	required_providers {
+		aws = {
+			source = "hashicorp/aws"
+			version = "3.11.0"
+		}
+	}
 }
 
 data "aws_organizations_organization" "org_root" {}
